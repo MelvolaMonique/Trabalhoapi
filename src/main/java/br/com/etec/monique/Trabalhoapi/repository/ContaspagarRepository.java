50812b603f3e9c50d4fce24a5efc60344d295b50
MelvolaMonique <monique.lima10@etec.gov.br>
2023-05-11 15:56:13 -0300

    erros arrumados e colocado a interface

---
+++ src/main/java/br/com/etec/monique/Trabalhoapi/repository/ContaspagarRepository.java
@@ -0,0 +1,7 @@
+package br.com.etec.monique.Trabalhoapi.repository;
+
+import br.com.etec.monique.Trabalhoapi.model.Contaspagar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContaspagarRepository extends JpaRepository<Contaspagar, Long> {
+}
